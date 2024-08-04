d3404013dd2d33068cb909f315ae61cfab699843
Dariya Kashtan <kashtandariya@gmail.com>
2024-08-04 17:33:57 +0200

    ELIMINATO TUTTO
    - Dopo aver litigato con il codice (non riuscendo a definire l'autore delle note tra admin e operatore) ho capito che dovevo fare una classe generica user e non separate. e gestire poi i ruoli. Ho eliminato tutto per rifarlo da 0

--- src/main/java/it/dkashtan/ticket_platform/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package it.dkashtan.ticket_platform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.dkashtan.ticket_platform.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-
-}
--- src/main/java/it/dkashtan/ticket_platform/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package it.dkashtan.ticket_platform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.dkashtan.ticket_platform.model.Note;
-
-public interface NoteRepository extends JpaRepository<Note, Integer>{
-
-}
